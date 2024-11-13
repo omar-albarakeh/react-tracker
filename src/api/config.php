2420aa4821328d5ab6af3a57ecb9c08c274710b3
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-13 21:45:14 +0200

    php <> config.php

---
+++ src/api/config.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$db_name = 'expense_tracker';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo json_encode(['error' => 'Database connection failed']);
+    exit();
+}
+?>
